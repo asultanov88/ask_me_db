2d5480fd944aa0358f64a1ca96f86ad1bfd7c634
CoolBean <uzsultanov@gmail.com>
2023-12-26 13:01:16 -0500

    TT delete.

--- dbo/user defiined types/ProviderTableType.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TYPE [ProviderTableType] AS TABLE
-( 
-	[ProviderId]	INT				NULL,
-	[FirstName]		NVARCHAR(255)	NOT NULL,
-	[LastName]		NVARCHAR(255)	NOT NULL,
-	[CreatedAt]		DATETIME2		NOT NULL
-)
