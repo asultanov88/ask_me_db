e5eb33ae1589a6a29b4fc0da90539a80581adbbc
CoolBean <uzsultanov@gmail.com>
2023-12-27 15:28:31 -0500

    LkCategory

---
+++ dbo/stored procedures/UspGetLkCategories.sql
@@ -0,0 +1,26 @@
+﻿-- ====================================================================================================================
+-- Author:      A. Sultanov
+-- Create Date: 12/04/2023
+-- Description: Gets LkCategory table lookup values.
+-- Modification History:
+-- When             Who                 Description 
+-- ------------     ----------------    -------------------------------------------------------------------------------
+-- ====================================================================================================================
+
+CREATE PROCEDURE [dbo].[UspGetLkCategories]
+
+AS
+BEGIN
+    SET NOCOUNT, XACT_ABORT ON;
+    BEGIN TRY
+        SELECT 
+            [LkCategoryId],
+            [Name]
+        FROM 
+            [dbo].[LkCategory]
+
+    END TRY
+    BEGIN CATCH
+        THROW
+    END CATCH
+END
