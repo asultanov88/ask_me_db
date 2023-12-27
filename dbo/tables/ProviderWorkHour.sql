d6a8dbcab710555ffbde3fa9967361ad857932b6
CoolBean <uzsultanov@gmail.com>
2023-12-27 15:32:03 -0500

    unique constrains

--- dbo/tables/ProviderWorkHour.sql
+++ dbo/tables/ProviderWorkHour.sql
@@ -4,6 +4,7 @@
 	[ProviderId]			INT NOT NULL,
 	[FromLkWorkHourId]		INT NOT NULL,
 	[ToLkWorkHourId]		INT NOT NULL,
+	CONSTRAINT UC_ProviderWorkHour UNIQUE(ProviderId),
 	CONSTRAINT [ProviderWorkHourId] PRIMARY KEY CLUSTERED ([ProviderWorkHourId] ASC),
 	CONSTRAINT [FK_ProviderWorkHour_FromLkWorkHourId] FOREIGN KEY ([FromLkWorkHourId]) REFERENCES [dbo].[LkWorkHour]([LkWorkHourId]),
 	CONSTRAINT [FK_ProviderWorkHour_ToLkWorkHourId] FOREIGN KEY ([ToLkWorkHourId]) REFERENCES [dbo].[LkWorkHour]([LkWorkHourId])
