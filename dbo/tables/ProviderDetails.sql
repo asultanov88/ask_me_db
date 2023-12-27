9dc53f3f2756854b51575619f9239883899a2ec4
CoolBean <uzsultanov@gmail.com>
2023-12-27 10:18:42 -0500

    Provider work details.

---
+++ dbo/tables/ProviderDetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ProviderDetails]
+(
+	[ProviderDetailsId] INT IDENTITY(1000, 1) NOT NULL,
+	[ProviderId]		INT NOT NULL,
+	[CompanyName]		NVARCHAR(255)	NOT NULL,
+	[Address]			NVARCHAR(1000)	NOT NULL,
+	[PhoneNumber]		NVARCHAR(50)	NOT NULL,
+	[Description]		NVARCHAR(1000)	NOT NULL,
+	CONSTRAINT [ProviderDetailsId] PRIMARY KEY CLUSTERED ([ProviderDetailsId] ASC),
+	CONSTRAINT [FK_ProviderDetails_ProviderId] FOREIGN KEY ([ProviderId]) REFERENCES [dbo].[ProviderUser]([ProviderId])
+)
---
+++ dbo/tables/ProviderWorkDay.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProviderWorkDay]
+(
+	[ProviderWorkDayId] INT IDENTITY(1000, 1) NOT NULL,
+	[ProviderId]		INT NOT NULL,
+	[LkWeekDayId]		INT NOT NULL,
+	CONSTRAINT [ProviderWorkDayId] PRIMARY KEY CLUSTERED ([ProviderWorkDayId] ASC),
+	CONSTRAINT [FK_ProviderWorkDay_ProviderId] FOREIGN KEY ([ProviderId]) REFERENCES [dbo].[ProviderUser]([ProviderId]),
+	CONSTRAINT [Fk_ProviderWorkDay_LkWeekDayId] FOREIGN KEY ([LkWeekDayId]) REFERENCES [dbo].[LkWeekDay]([LkWeekDayId]),
+)
