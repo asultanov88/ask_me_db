8c93fb83c0c2cba79541b99959e8e1267ec6449f
CoolBean <uzsultanov@gmail.com>
2023-12-27 15:36:10 -0500

    more constraints

--- dbo/tables/ClientUser.sql
+++ dbo/tables/ClientUser.sql
@@ -3,5 +3,6 @@
 	[ClientId]	INT IDENTITY(1000, 1) NOT NULL,
 	[UserId]	INT NOT NULL,
 	CONSTRAINT [ClientId] PRIMARY KEY CLUSTERED ([ClientId] ASC),
+	CONSTRAINT UC_ClientUser UNIQUE(UserId),
 	CONSTRAINT [FK_ClientUser_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([UserId])
 )
--- dbo/tables/ProviderUser.sql
+++ dbo/tables/ProviderUser.sql
@@ -3,5 +3,6 @@
 	[ProviderId]	INT IDENTITY(1000, 1) NOT NULL,
 	[UserId]		INT NOT NULL,
 	CONSTRAINT [ProviderId] PRIMARY KEY CLUSTERED ([ProviderId] ASC),
+	CONSTRAINT UC_ProviderUser UNIQUE(UserId),
 	CONSTRAINT [FK_ProvidertUser_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([UserId])
 )
