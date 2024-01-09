7ebc1319ce8c8211e6b5b8098018bd9a9b7ed02f
CoolBean <uzsultanov@gmail.com>
2024-01-08 23:22:28 -0500

    update refactor.

--- dbo/stored procedures/UspUpdateMessageViewed.sql
+++ dbo/stored procedures/UspUpdateMessageViewed.sql
@@ -29,6 +29,25 @@ BEGIN
                 RAISERROR(@Error,16,1)
             END
 
+        SELECT TOP 1
+            m.[MessageId],
+            cu.UserId AS ClientUserId,
+            pu.[UserId] AS ProviderUserId
+        FROM
+            [dbo].[Message] m
+            JOIN [SubjectMessage] sm
+                ON sm.[MessageId] = m.[MessageId]
+            JOIN [Subject] s
+                ON s.[SubjectId] = sm.[SubjectId]
+            JOIN [ClientProvider] cp
+                ON cp.[ClientProviderId] = s.[ClientProviderId]
+            JOIN [ClientUser] cu
+                ON cu.[ClientId] = cp.[ClientId]
+            JOIN [ProviderUser] pu
+                ON pu.[ProviderId] = cp.[ProviderId]
+        WHERE
+            m.[MessageId] = @MessageId
+
     END TRY
     BEGIN CATCH
         THROW
