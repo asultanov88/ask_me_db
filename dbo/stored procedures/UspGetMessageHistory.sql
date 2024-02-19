38214bc0532ee1a4f99c47c6a119d5bb6dc35eed
CoolBean <uzsultanov@gmail.com>
2024-02-19 16:31:42 -0500

    message history

---
+++ dbo/stored procedures/UspGetMessageHistory.sql
@@ -0,0 +1,36 @@
+﻿-- ====================================================================================================================
+-- Author:      A. Sultanov
+-- Create Date: 12/04/2023
+-- Description: Gets message history.
+-- Modification History:
+-- When             Who                 Description 
+-- ------------     ----------------    -------------------------------------------------------------------------------
+-- ====================================================================================================================
+
+CREATE PROCEDURE [dbo].[UspGetMessageHistory]
+
+    @MessageIds PkTableType READONLY
+
+AS
+BEGIN
+    SET NOCOUNT, XACT_ABORT ON;
+    BEGIN TRY
+              
+        SELECT
+            mh.[MessageHistoryId],
+            mh.[MessageId],
+            mh.[Message],
+            mh.[CreatedAt]
+        FROM
+            [dbo].[MessageHistory] mh
+            JOIN @MessageIds mids
+                ON mh.[MessageId] = mids.[Pk]
+                AND mh.[Message] IS NOT NULL
+        ORDER BY
+            mh.[MessageHistoryId] DESC
+
+    END TRY
+    BEGIN CATCH
+        THROW
+    END CATCH
+END
